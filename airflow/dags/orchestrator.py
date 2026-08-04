e8449a71a81a6dd007322e4e90c6b54060349df6
AramuLucky <aramu7976@gmail.com>
2026-08-04 15:47:51 +0700

    add dbt

---
+++ airflow/dags/orchestrator.py
@@ -0,0 +1,25 @@
+import sys
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime,timedelta
+
+sys.path.append('/opt/airflow/api-request') 
+from insert_records import main
+
+default_args = {
+    'description':'A DAG to orchestrate data',
+    'start_date':datetime(2026,8,3),
+    'catchup': False,
+}
+
+dag = DAG(
+    dag_id = 'weather-api-orchestrator',
+    default_args = default_args,
+    schedule = timedelta (minutes = 5)
+)
+
+with dag:
+    task1 = PythonOperator(
+        task_id = 'ingest_data_task',
+        python_callable = main
+    )
